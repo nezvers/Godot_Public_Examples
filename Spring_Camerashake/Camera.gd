4f4bc971275a989d690738b45b31bdd49c23e356
nezvers <agnis16@inbox.lv>
2019-12-03 21:16:04 +0200

    Camerashake based on Hook's Law
    Best camerashake I've found.
    Easily adaptable to other game engines

---
+++ Spring_Camerashake/Camera.gd
@@ -0,0 +1,49 @@
+
+extends Camera2D
+
+#Variables
+export (float) var Tension: float = 0.025		setget set_tension
+export (float) var Dampening:float = 0.025		setget set_dampening
+export (float) var DampingRatio:float = 1.0		setget set_dampingRatio
+var TargetPosition: Vector2 = Vector2.ZERO
+var CurrentPosition:Vector2 = TargetPosition
+var Velocity:Vector2 = Vector2.ZERO
+
+func set_dampening(value:float)->void:
+	Dampening = value
+	Tension = Dampening / DampingRatio
+	print("Dampening:", Dampening, ", Tension:", Tension, " ,Ratio:", DampingRatio)
+
+func set_tension(value:float)->void:
+	Tension = value
+	Dampening = Tension * DampingRatio
+	print("Dampening:", Dampening, ", Tension:", Tension, " ,Ratio:", DampingRatio)
+
+func set_dampingRatio(value:float)->void:
+	if value == 0:
+		return
+	DampingRatio = value
+	Dampening = Tension * DampingRatio
+	Tension = Dampening / DampingRatio
+	print("Dampening:", Dampening, ", Tension:", Tension, " ,Ratio:", DampingRatio)
+
+func _ready()->void:
+	Event.connect("Camerashake", self, "apply_camerashake")
+
+func _unhandled_input(event)->void:
+	if event.is_action_pressed("click"):
+		var strength: = 100.0
+		var impulse: = Vector2(-strength/2 +(randf()*strength), -strength/2 +(randf()*strength))
+		Event.emit_signal("Camerashake", impulse)
+
+func _process(delta)->void:
+	camerashake()
+	position = CurrentPosition
+
+func camerashake()->void:
+	var Displacement: = (TargetPosition - CurrentPosition)
+	Velocity += (Tension * Displacement) - (Dampening * Velocity)
+	CurrentPosition += Velocity
+
+func apply_camerashake(impulse:Vector2)->void:
+	Velocity += impulse
