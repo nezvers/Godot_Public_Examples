0c56986d291eecd4d1ac7d18c824be0bd0bcb171
nezvers <agnis16@inbox.lv>
2020-09-03 23:28:39 +0300

    Pendulum example update

--- Nature_code/Pendulum/Objects/Pendulum.gd
+++ Nature_code/Pendulum/Objects/Pendulum.gd
@@ -1,23 +1,27 @@
 extends Position2D
+#class_name Pendulum
 
-onready var Origin:Vector2 = global_position 				#point the pendulum rotates around
-export (Vector2) var Pendulum_position: = Vector2() 		#pendulum itself
-onready var arm_length = Vector2.ZERO.distance_to(Pendulum_position-Origin)
-onready var angle = Vector2.ZERO.angle_to(Pendulum_position-Origin) - deg2rad(-90) #Get angle between position + add godot angle offset
+var pivot_point:Vector2				 				#point the pendulum rotates around
+export (Vector2) var end_position: = Vector2() 		#pendulum itself
+var arm_length:float
+var angle											#Get angle between position + add godot angle offset
+
 export (float) var gravity = 0.4 * 60
 export (float) var damping = 0.995 							#Arbitrary dampening force
 
 var angular_velocity = 0.0
 var angular_acceleration = 0.0
 
-func _draw()->void:
-	draw_line(Vector2.ZERO, Pendulum_position - Origin, Color.white, 1.0, false)
+func _ready()->void:
+	set_start_position(global_position, end_position)
 
-func _physics_process(delta)->void:
-	game_input()											#example of in game swing kick
-	
-	process_velocity(delta)
-	update()												#draw
+func set_start_position(start_pos:Vector2, end_pos:Vector2):
+	pivot_point = start_pos
+	end_position = end_pos
+	arm_length = Vector2.ZERO.distance_to(end_position-pivot_point)
+	angle = Vector2.ZERO.angle_to(end_position-pivot_point) - deg2rad(-90)
+	angular_velocity = 0.0
+	angular_acceleration = 0.0
 
 func process_velocity(delta:float)->void:
 	angular_acceleration = ((-gravity*delta) / arm_length) *sin(angle)	#Calculate acceleration (see: http://www.myphysicslab.com/pendulum1.html)
@@ -25,10 +29,16 @@ func process_velocity(delta:float)->void:
 	angular_velocity *= damping								#Arbitrary damping
 	angle += angular_velocity								#Increment angle
 	
-	Pendulum_position = Origin + Vector2(arm_length*sin(angle), arm_length*cos(angle))
+	end_position = pivot_point + Vector2(arm_length*sin(angle), arm_length*cos(angle))
 
 func add_angular_velocity(force:float)->void:
 	angular_velocity += force
+
+func _physics_process(delta)->void:
+	game_input()											#example of in game swing kick
+	
+	process_velocity(delta)
+	update()												#draw
 
 func game_input()->void:
 	var dir:float = 0
@@ -36,4 +46,8 @@ func game_input()->void:
 		dir += 1
 	elif Input.is_action_just_pressed("left"):
 		dir -= 1
-	add_angular_velocity(dir * 0.18) 						#give a kick to the swing
+	add_angular_velocity(dir * 0.02) 						#give a kick to the swing
+
+func _draw()->void:
+	draw_line(Vector2.ZERO, end_position - pivot_point, Color.white, 1.0, false)
+	draw_circle(end_position - pivot_point, 3.0, Color.red)
