3bf4547577fc9d4a3632e2844e1512f185a7d1ec
nezvers <agnis16@gmail.com>
2021-04-05 17:33:02 +0300

    Update Verlet integration example

--- Nature_code/Verlet Rope/string.gd
+++ Nature_code/Verlet Rope/string.gd
@@ -1,77 +1,97 @@
 extends Node2D
 
 
-export (float) var length = 30
-export (float) var constrain = 1
+export (float) var ropeLength = 30
+export (float) var constrain = 1	# distance between points
 export (Vector2) var gravity = Vector2(0,9.8)
-export (float) var friction = 0.9
-export (bool) var start_pin = true
-export (bool) var end_pin = true
+export (float) var dampening = 0.9
+export (bool) var startPin = true
+export (bool) var endPin = true
+
+onready var line2D: = $Line2D
 
 var pos: PoolVector2Array
-var pos_ex: PoolVector2Array
-var count: int
+var posPrev: PoolVector2Array
+var pointCount: int
 
-func _ready():
-	count = get_count(length)
+func _ready()->void:
+	pointCount = get_pointCount(ropeLength)
 	resize_arrays()
 	init_position()
 
-func get_count(distance: float):
-	var new_count = ceil(distance / constrain)
-	return new_count
+func get_pointCount(distance: float)->int:
+	return int(ceil(distance / constrain))
 
 func resize_arrays():
-	pos.resize(count)
-	pos_ex.resize(count)
+	pos.resize(pointCount)
+	posPrev.resize(pointCount)
 
-func init_position():
-	for i in range(count):
+func init_position()->void:
+	for i in range(pointCount):
 		pos[i] = position + Vector2(constrain *i, 0)
-		pos_ex[i] = position + Vector2(constrain *i, 0)
+		posPrev[i] = position + Vector2(constrain *i, 0)
 	position = Vector2.ZERO
 
-func _process(delta):
-	if Input.is_action_pressed("click"):	#Move start point
-		pos[0] = get_global_mouse_position()
-		pos_ex[0] = get_global_mouse_position()
-		#find the best feel length ratio
-		print("length ratio to distance ",length/pos[count-1].distance_to(pos[0]))
-	if Input.is_action_pressed("right_click"):	#Move start point
-		pos[count-1] = get_global_mouse_position()
-		pos_ex[count-1] = get_global_mouse_position()
+func _unhandled_input(event:InputEvent)->void:
+	if event is InputEventMouseMotion:
+		if Input.is_action_pressed("click"):	#Move start point
+			set_start(get_global_mouse_position())
+		if Input.is_action_pressed("right_click"):	#Move start point
+			set_last(get_global_mouse_position())
+	elif event is InputEventMouseButton && event.is_pressed():
+		if event.button_index == 1:
+			set_start(get_global_mouse_position())
+		elif event.button_index == 2:
+			set_last(get_global_mouse_position())
+
+func _process(delta)->void:
 	update_points(delta)
-	update_distance()
-	#update_distance()	#Repeat to get tighter rope
-	#update_distance()
-	$Line2D.points = pos
+	update_constrain()
+	
+	#update_constrain()	#Repeat to get tighter rope
+	#update_constrain()
+	
+	# Send positions to Line2D for drawing
+	line2D.points = pos
 
-func update_points(delta):
-	for i in range (count):
+func set_start(p:Vector2)->void:
+	pos[0] = p
+	posPrev[0] = p
+
+func set_last(p:Vector2)->void:
+	pos[pointCount-1] = p
+	posPrev[pointCount-1] = p
+
+func update_points(delta)->void:
+	for i in range (pointCount):
 		# not first and last || first if not pinned || last if not pinned
-		if (i!=0 && i!=count-1) || (i==0 && !start_pin) || (i==count-1 && !end_pin):
-			var vec2 = (pos[i] - pos_ex[i]) * friction
-			pos_ex[i] = pos[i]
-			pos[i] += vec2 + (gravity * delta)
+		if (i!=0 && i!=pointCount-1) || (i==0 && !startPin) || (i==pointCount-1 && !endPin):
+			var velocity = (pos[i] -posPrev[i]) * dampening
+			posPrev[i] = pos[i]
+			pos[i] += velocity + (gravity * delta)
 
-func update_distance():
-	for i in range(count):
-		if i == count-1:
+func update_constrain()->void:
+	for i in range(pointCount):
+		if i == pointCount-1:
 			return
 		var distance = pos[i].distance_to(pos[i+1])
 		var difference = constrain - distance
 		var percent = difference / distance
 		var vec2 = pos[i+1] - pos[i]
+		
+		# if first point
 		if i == 0:
-			if start_pin:
+			if startPin:
 				pos[i+1] += vec2 * percent
 			else:
 				pos[i] -= vec2 * (percent/2)
 				pos[i+1] += vec2 * (percent/2)
-		elif i == count-1:
+		# if last point, skip because no more points after it
+		elif i == pointCount-1:
 			pass
+		# all the rest
 		else:
-			if i+1 == count-1 && end_pin:
+			if i+1 == pointCount-1 && endPin:
 				pos[i] -= vec2 * percent
 			else:
 				pos[i] -= vec2 * (percent/2)
