d990054d15525611a628f947bff00877685ae086
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2022-06-27 17:15:23 +0300

    Update Player.gd

--- Boomerang/Player/Player.gd
+++ Boomerang/Player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 export var max_speed_default: = Vector2(120.0, 350.0) 	#speed clamp X & Y
-export var acceleration_default: = Vector2(240, 1000.0)	#Acceleration & gravity
+export var acceleration_default: = Vector2(8.0, 1000.0)	#Acceleration & gravity
 export var jump_impulse: = 350.0						#Jump force
 export var friction_default: = 0.15						#Slipery stop
 
@@ -23,11 +23,7 @@ func _physics_process(delta):
 func calculate_velocity(delta: float) -> Vector2:
 	var new_velocity: = velocity #Use temporary variable in case we want to manipulate the value later
 	#HORIZONTAL
-	if direction.x!=0:
-		new_velocity.x += direction.x * acceleration.x * delta
-		new_velocity.x = clamp(new_velocity.x, -max_speed.x, max_speed.x)
-	else: #deaccelerate X
-		new_velocity.x = approach(new_velocity.x, 0, abs(new_velocity.x*friction))
+	new_velocity.x = lerp(new_velocity.x, direction.x * max_speed.x, acceleration.x * delta)
 	
 	#VERTICAL
 	new_velocity.y += acceleration.y * delta
@@ -42,6 +38,6 @@ static func get_move_direction() -> Vector2:
 
 static func approach(start: float, end: float, amount: float):
 	if (start < end):
-	    return min(start + amount, end)
+		return min(start + amount, end)
 	else:
-	    return max(start - amount, end)
+		return max(start - amount, end)
