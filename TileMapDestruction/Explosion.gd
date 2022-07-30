849bff997d8e53fa5b47498c18bd8f3f27d1be20
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2022-07-30 14:07:59 +0300

    Remove possible memory leak

--- TileMapDestruction/Explosion.gd
+++ TileMapDestruction/Explosion.gd
@@ -1,15 +1,15 @@
 class_name Explosion
 
-var size1: = [
+const size1: = [
 	Vector2(0,0)
 ]
-var size2: = [
+const size2: = [
 	Vector2(1,	0),
 	Vector2(-1,	0),
 	Vector2(0,	1),
 	Vector2(0,	-1)
 ]
-var size3: = [
+const size3: = [
 	Vector2(1,	1),
 	Vector2(-1,	1),
 	Vector2(1,	-1),
@@ -19,7 +19,7 @@ var size3: = [
 	Vector2(0,2),
 	Vector2(0,-2)
 ]
-var size4: = [
+const size4: = [
 	Vector2(0, -3),
 	Vector2(-2, -2),
 	Vector2(-1, -2),
@@ -38,21 +38,28 @@ var size4: = [
 	Vector2(2, 1)
 ]
 
-var shapes: = [
+const shapes: = [
 	size1,
 	size2,
 	size3,
 	size4
 ]
 
-
-func _init(map:TileMap, _position:Vector2)->void:
+# static function doesn't require instancing
+static func trigger(map:TileMap, _position:Vector2)->void:
 	var map_position: Vector2 = map.world_to_map(map.to_local(_position))
 	for shape in shapes:
 		for pos in shape:
 			map.set_cellv(map_position + pos, -1)
-		yield(map.get_tree().create_timer(0.017), "timeout")
+		# Yields for animations
+			#yield(map.get_tree().create_timer(0.008), "timeout")
+		yield(map.get_tree().create_timer(0.08), "timeout")
 
+# control animation and size from caller
+static func trigger_shape(map:TileMap, _position:Vector2, i:int)->void:
+	var map_position: Vector2 = map.world_to_map(map.to_local(_position))
+	for pos in shapes[i % shapes.size()]:
+			map.set_cellv(map_position + pos, -1)
 
 
 
--- TileMapDestruction/Level.gd
+++ TileMapDestruction/Level.gd
@@ -8,13 +8,16 @@ func _unhandled_input(event):
 			#Explosion.new(tilemap, get_global_mouse_position())
 			random_explosions()
 	elif event.is_action_pressed("ui_cancel"):
+# warning-ignore:return_value_discarded
 		get_tree().reload_current_scene()
 
 func random_explosions()->void:
+# warning-ignore:unused_variable
 	var pos: = get_global_mouse_position()
 	var rand_size: = 80.0
 	var count: = 6
 	
 	for i in count:
 		var rand_pos: = Vector2(rand_range(-rand_size, rand_size), rand_range(-rand_size, rand_size))
-		Explosion.new(tilemap, get_global_mouse_position() + rand_pos)
+		# call static function that doesn't need instancing
+		Explosion.trigger(tilemap, get_global_mouse_position() + rand_pos)
